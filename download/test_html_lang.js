a8a6fbd154421e9126c6fa48f306e0e7a64b1e4e
andrzejQ <kubaszek@prz.edu.pl>
2021-03-30 08:43:17 +0200

    bundle update

---
+++ download/test_html_lang.js
@@ -0,0 +1,10 @@
+﻿  function getEl(ev) {
+    ov.style.pointerEvents = 'none';
+    var el = d.elementFromPoint(ev.clientX, ev.clientY);
+    if (ev.shiftKey) {
+      var el0 = el, i=4; //4-1=3 parents up
+      while ((el0) && (i--)) {el = el0; el0 = el.parentElement;}
+    }
+    ov.style.pointerEvents = 'auto';
+    return el;
+  }
